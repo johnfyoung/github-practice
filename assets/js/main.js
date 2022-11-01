28c7a0612ba1875030125c3813dd34639162eb3b
John Young <john@codeandcreative.com>
2022-10-31 18:23:19 -0700

    Adds button to front page
    Resolves #2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,17 @@
 // HOOKS
 var headingEl = document.querySelector("h1");
+var buttonEl = document.querySelector("#thebutton");
 
+// state
+var clickCount = 0;
+
+// Event Listeners
+buttonEl.addEventListener("click", function (ev) {
+  clickCount++;
+  alert(clickCount + " click(s)");
+});
+
+// Main Function
 function init() {
   headingEl.textContent = "This is from the Javascript file";
 }
