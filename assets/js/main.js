1b165650fd53b94d8c65658b51681bc2469592b6
John Young <john@codeandcreative.com>
2022-10-31 18:18:55 -0700

    Changes the javascript to the heading

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 var headingEl = document.querySelector("h1");
 
 function init() {
-  headingEl.textContent = "This is from the Javascript file";
+  headingEl.textContent = "This is from the Javascript file and some more";
 }
 
 init();
